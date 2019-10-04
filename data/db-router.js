3075ae572b387335acdd3c9743af916b75b04afb
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-10-04 12:24:43 -0400

    Delete request

--- data/db-router.js
+++ data/db-router.js
@@ -7,15 +7,39 @@ const act = require('./helpers/actionModel');
 const router = express.Router();
 
 
+//POST
 
-router.get('/', (req,res)=>{
+
+// GET 
+router.get('/:id', (req,res)=>{
     pro.get(req.params.id)
     .then(pro => {
     res.status(200).json(pro)
     })
 });
 
+//PUT
+router.post('/', (req, res)=> {
+    pro.update(req.body)
+    .then(pro => {
+        res.status(201).json(pro);
+    })
+    .catch(error => {
+        // log error to server
+        console.log(error);
+        res.status(500).json({
+          message: 'Error updating the project',
+        });
+      });
+})
 
+//DELETE
+router.delete('/actions/:id', (req, res)=>{
+    act.remove(req.params.id)
+    .then(pro => {
+        res.status(200).json(pro)
+    })
+})
 
 
 
